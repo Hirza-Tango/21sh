a0ef0a542cf12624454544303132d10a50016bb0
Dylan Slogrove <dslogrove@gmail.com>
2018-09-11 11:37:04 +0200

    Fixed single quote checking

--- token_parser/token_parser.c
+++ token_parser/token_parser.c
@@ -6,16 +6,16 @@
 /*   By: dslogrov <dslogrove@gmail.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/10 13:56:48 by dslogrov          #+#    #+#             */
-/*   Updated: 2018/09/10 16:47:50 by dslogrov         ###   ########.fr       */
+/*   Updated: 2018/09/11 11:33:53 by dslogrov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "token_parser.h"
 
-char	*token_recognise(char **input)
+/*char	*token_recognise(char **input)
 {
 	PASS;
-}
+}*/
 
 char	quote_check(const char *s)
 {
@@ -28,9 +28,7 @@ char	quote_check(const char *s)
 			dup[1] ? dup++ : PASS;
 		else if (*dup == '\'')
 		{
-			while ((dup = ft_strchr(dup + 1, '\'')))
-				PASS;
-			if (!dup)
+			if (!(dup = ft_strchr(dup + 1, '\'')))
 				return ('\'');
 		}
 		else if (*dup == '\"')
@@ -48,15 +46,17 @@ char	quote_check(const char *s)
 char	*token_list(const char *prompt)
 {
 	char		*result;
-	char		*token;
+	//char		*token;
 	t_d_list	*list;
 
 	result = ft_readline(prompt);
 	while (quote_check(result))
+	{
 		ft_swapnfree((void **)&result,
 		ft_strmjoin(3, result, "\n", ft_readline("> ")));
+	}
 	list = NULL;
-	while ((token = token_recognise(result)))
-		ft_dlstadd(&list, ft_dlstnew(token, TOK_TOKEN));
+	//while ((token = token_recognise(result)))
+	//	ft_dlstadd(&list, ft_dlstnew(token, TOK_TOKEN));
 	return (result);
 }
