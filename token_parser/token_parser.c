6e85d79efc116041ac3f67e10d9819a18cede617
Dylan Slogrove <dslogrove@gmail.com>
2018-09-12 14:59:22 +0200

    Some preliminary operator parsing

--- token_parser/token_parser.c
+++ token_parser/token_parser.c
@@ -6,7 +6,7 @@
 /*   By: dslogrov <dslogrove@gmail.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/10 13:56:48 by dslogrov          #+#    #+#             */
-/*   Updated: 2018/09/11 18:52:07 by dslogrov         ###   ########.fr       */
+/*   Updated: 2018/09/12 14:53:04 by dslogrov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,16 +25,97 @@ t_list	*ft_lstfakenew(void *content, size_t content_size)
 	return (ret);
 }
 
-int		ft_isop(char s)
+int		redir_type(char *s)
 {
-	if (s == '|' || s == '&' || s == ';' || s == '<' || s == '>')
+	while (ft_isdigit(*s))
+		s++;
+	if (s[0] == '<')
+	{
+		if (s[1] == '<')
+			return (s[2] == '-' ? T_DLESSDASH : T_DLESS);
+		else if (s[1] == '&')
+			return (T_LESSAND);
+		else if (s[1] == '>')
+			return (T_LESSGREAT);
+		return (T_LESS);
+	}
+	else if (*s == '>')
+	{
+		if (s[1] == '>')
+			return (T_DGREAT);
+		else if (s[1] == '&')
+			return (T_GREATAND);
+		else if (s[1] == '|')
+			return (T_CLOBBER);
+		return (T_GREAT);
+	}
+	return (0);
+}
+
+int		ft_is_redir_word(char *s)
+{
+	char *dup;
+
+	dup = s;
+	while (ft_isdigit(*dup))
+		dup++;
+	if (*dup == '<')
+	{
+		dup++;
+		if (*dup == '<' || *dup == '>' || *dup == '&')
+		{
+			if (*dup == '<' && dup[1] == '-')
+				return (dup - s + 2);
+			return (dup - s + 1);
+		}
+		return (dup - s);
+	}
+	else if (*dup == '>')
+	{
+		dup++;
+		if (*dup == '|' || *dup == '>' || *dup == '&')
+			return (dup - s + 1);
+		return (dup - s);
+	}
+	return (0);
+}
+
+void	tok_operator(char **s, t_list **list)
+{
+	if (ft_is_redir_word(*s))
+	{
+		ft_lstappend(list, ft_lstfakenew(
+			ft_strndup(*s, ft_is_redir_word(*s)), redir_type(*s)));
+		*s += ft_is_redir_word(*s);
+	}
+	else if ((**s == '|' || **s == '&' || **s == ';') && (*s)[0] == (*s)[1])
+	{
+		if (**s == '|')
+			ft_lstappend(list, ft_lstfakenew(ft_strndup((*s)++, 2), T_OR_IF));
+		else if (**s == '&')
+			ft_lstappend(list, ft_lstfakenew(ft_strndup((*s)++, 2), T_AND_IF));
+		else if (**s == ';')
+			ft_lstappend(list, ft_lstfakenew(ft_strndup((*s)++, 2), T_DSEMI));
+		(*s)++;
+	}
+	else if (**s == '|')
+		ft_lstappend(list, ft_lstfakenew(ft_strndup((*s)++, 1), T_PIPE));
+	else if (**s == '&')
+		ft_lstappend(list, ft_lstfakenew(ft_strndup((*s)++, 1), T_AMP));
+	else if (**s == ';')
+		ft_lstappend(list, ft_lstfakenew(ft_strndup((*s)++, 1), T_SEMI));
+}
+
+int		ft_is_op_char(char c)
+{
+	if (c == '|' || c == '&' || c == ';' || c == '<' || c == '>')
 		return (1);
 	return (0);
 }
 
 void	tok_newline(char **s, t_list **list)
 {
-	ft_lstappend(list, ft_lstfakenew(ft_strndup(*s, 1), TOK_NEWLINE));
+	ft_lstappend(list, ft_lstfakenew(ft_strndup(*s, 1), T_NEWLINE));
 	*s += 1;
 }
 
@@ -48,12 +129,11 @@ void	tok_dquote(char **s, t_list **list)
 	while ((*s)[i] != '\"' && (*s)[i - 1] != '\\')
 	{
 		if (!(*s)[i])
-			ft_swapnfree((void **)s, ft_strmjoin(3, *s, "\n", ft_readline("> ")));
+			ft_swapnfree((void **)s,
+				ft_strmjoin(3, *s, "\n", ft_readline("> ")));
 		i++;
 	}
-	ft_lstappend(list, ft_lstfakenew(ft_strndup(*s, i + 1), TOK_DQUOTE));
-	ft_putstr("LOG: ");
-	ft_putendl((*list)->content);
+	ft_lstappend(list, ft_lstfakenew(ft_strndup(*s, i + 1), T_DQUOTE));
 	*s += i + 1;
 }
 
@@ -67,10 +147,11 @@ void	tok_squote(char **s, t_list **list)
 	while ((*s)[i] != '\'')
 	{
 		if (!(*s)[i])
-			ft_swapnfree((void **)s, ft_strmjoin(3, *s, "\n", ft_readline("> ")));
+			ft_swapnfree((void **)s,
+				ft_strmjoin(3, *s, "\n", ft_readline("> ")));
 		i++;
 	}
-	ft_lstappend(list, ft_lstfakenew(ft_strndup(*s, i + 1), TOK_SQUOTE));
+	ft_lstappend(list, ft_lstfakenew(ft_strndup(*s, i + 1), T_SQUOTE));
 	*s += i + 1;
 }
 
@@ -85,41 +166,63 @@ void	tok_word(char **s, t_list **list)
 	{
 		if ((*dup == '\"' || *dup == '\'') && prev != '\\')
 			break ;
-		if (ft_isop(*dup) || ft_isspace(*dup))
+		if (ft_is_op_char(*dup) || ft_isspace(*dup))
 			break ;
 		if (*dup == '$' || *dup == '`' || *dup == '#')
 			break ;
 		prev = *dup;
 		dup++;
 	}
-	ft_lstappend(list, ft_lstfakenew(ft_strndup(*s, dup - *s), TOK_WORD));
+	ft_lstappend(list, ft_lstfakenew(ft_strndup(*s, dup - *s), T_WORD));
 	(void)list;
+	*s = dup;
+}
+
+void	tok_space(char **s, t_list **list)
+{
+	while (ft_isspace(**s) && **s != '\n')
+		(*s)++;
+	ft_lstappend(list, ft_lstfakenew(ft_strdup(" "), T_SPACE));
+}
+
+void	tok_comment(char **s, t_list **list)
+{
+	char *dup;
+
+	if (**s != '#')
+		return ;
+	dup = *s + 1;
+	while (*dup != '\n' && *dup)
+		dup++;
+	ft_lstappend(list, ft_lstfakenew(ft_strndup(*s, dup - *s), T_COMMENT));
 	*s = dup;
 }
 
 t_list	*split_tokens(char *result)
 {
 	t_list	*list;
+	char	*dup;
 
 	list = NULL;
-	while (*result)
+	dup = result;
+	while (*dup)
 	{
-		if (*result == '\"')
-			tok_dquote(&result, &list);
-		else if (*result == '\'')
-			tok_squote(&result, &list);
-		//else if (ft_isop(*result))
-		//	tok_operator(&result, &list);
-		else if (*result == '\n')
-			tok_newline(&result, &list);
-		else if (ft_isspace(*result))
-			result++;
-		//else if (*result == '$' || *result == '`')
-		//	tok_subst(&result, &list);
-		//else if (*result == '#')
-		//	tok_comment(&result, &list);
+		if (*dup == '\"')
+			tok_dquote(&dup, &list);
+		else if (*dup == '\'')
+			tok_squote(&dup, &list);
+		else if (ft_is_op_char(*dup) || ft_is_redir_word(dup))
+			tok_operator(&dup, &list);
+		else if (*dup == '\n')
+			tok_newline(&dup, &list);
+		else if (ft_isspace(*dup))
+			tok_space(&dup, &list);
+		//else if (*dup == '$' || *dup == '`')
+		//	tok_subst(&dup, &list);
+		else if (*dup == '#')
+			tok_comment(&dup, &list);
 		else
-			tok_word(&result, &list);
+			tok_word(&dup, &list);
 	}
 	return (list);
 }
@@ -152,6 +255,30 @@ char	quote_check(const char *s)
 	return (0);
 }
 
+void	list_print(t_list *elem)
+{
+	ft_putstr("LOG: TYPE: ");
+	switch (elem->content_size)
+	{
+		case T_WORD :
+			ft_putstr("WORD"); break;
+		case T_DQUOTE :
+			ft_putstr("DQUOTE"); break;
+		case T_SQUOTE :
+			ft_putstr("SQUOTE"); break;
+		case T_NEWLINE :
+			ft_putstr("NEWLINE"); break;
+		case T_SPACE :
+			/*ft_putstr("SPACE");*/ break;
+		case T_COMMENT :
+			ft_putstr("COMMENT"); break;
+		default :
+			ft_putstr("OTHER");
+	}
+	ft_putstr(" CONTENT: ");
+	ft_putendl(elem->content);
+}
+
 char	*token_list(const char *prompt)
 {
 	char	*result;
@@ -159,5 +286,7 @@ char	*token_list(const char *prompt)
 
 	result = ft_readline(prompt);
 	split = split_tokens(result);
-	return (result);
+	ft_lstiter(split, list_print);
+	ft_putendl("LOG: Done parsing");
+	return (ft_strdup(""));
 }
--- token_parser/token_parser.h
+++ token_parser/token_parser.h
@@ -6,7 +6,7 @@
 /*   By: dslogrov <dslogrove@gmail.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/10 13:56:04 by dslogrov          #+#    #+#             */
-/*   Updated: 2018/09/11 18:44:18 by dslogrov         ###   ########.fr       */
+/*   Updated: 2018/09/12 14:14:23 by dslogrov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,38 +17,45 @@
 
 enum	e_tokens
 {
-	TOK_END = 0,
-	TOK_DQUOTE,
-	TOK_SQUOTE,
-	TOK_TOKEN,
-	TOK_NEWLINE,
-	TOK_WORD,
-	TOK_NAME,
-	TOK_ASSIGNMENT_WORD,
-	TOK_IO_NUMBER,
-	TOK_AND_IF,
-	TOK_OR_IF,
-	TOK_DSEMI,
-	TOK_DLESS,
-	TOK_DGREAT,
-	TOK_LESSAND,
-	TOK_GREATAND,
-	TOK_LESSGREAT,
-	TOK_DLESSDASH,
-	TOK_CLOBBER,
-	TOK_IF,
-	TOK_THEN,
-	TOK_ELSE,
-	TOK_ELIF,
-	TOK_FI,
-	TOK_DO,
-	TOK_DONE,
-	TOK_CASE,
-	TOK_ESAC,
-	TOK_WHILE,
-	TOK_UNTIL,
-	TOK_FOR,
-	TOK_LBRACE
+	T_END = 0,
+	T_DQUOTE,
+	T_SQUOTE,
+	T_SPACE,
+	T_COMMENT,
+	T_TOKEN,
+	T_NEWLINE,
+	T_WORD,
+	T_NAME,
+	T_ASSIGNMENT_WORD,
+	T_IO_NUMBER,
+	T_AND_IF,
+	T_OR_IF,
+	T_GREAT,
+	T_LESS,
+	T_PIPE,
+	T_AMP,
+	T_SEMI,
+	T_DSEMI,
+	T_DLESS,
+	T_DGREAT,
+	T_LESSAND,
+	T_GREATAND,
+	T_LESSGREAT,
+	T_DLESSDASH,
+	T_CLOBBER,
+	T_IF,
+	T_THEN,
+	T_ELSE,
+	T_ELIF,
+	T_FI,
+	T_DO,
+	T_DONE,
+	T_CASE,
+	T_ESAC,
+	T_WHILE,
+	T_UNTIL,
+	T_FOR,
+	T_LBRACE
 };
 
 char	*token_list(const char *prompt);
